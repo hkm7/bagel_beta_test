176eb9ec99d7b6439ab6bcedfc1aa7fce02a2f86
Hrishikesh Mohan <50671806+hkm7@users.noreply.github.com>
2025-10-22 15:42:19 +0530

    Create all.tf

---
+++ all.tf
@@ -0,0 +1,79 @@
+#######
+# main.tf
+######
+resource "aws_instance" "web_server" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name              = var.key_pair_name
+  vpc_security_group_ids = var.security_groups
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+#########
+## Output.tf
+#########
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web_server.id
+}
+
+#########
+# Provider.tf
+########
+# Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+##############
+# variables.tf
+############
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "demo-web-server"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_pair_name" {
+  description = "Name of the AWS key pair for SSH access"
+  type        = string
+  default     = "ansible-test"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0360c520857e3138f"  # ubuntu24
+}
+
+variable "security_groups"{
+  type = list(string)
+  default = ["sg-0fc5e7a0917c11c50"]
+  description = "Enter security group ids"
+}
+
+
